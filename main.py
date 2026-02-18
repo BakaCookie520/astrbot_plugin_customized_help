4fde189d90d3fef8d61e3aa7aacfb6d52f42a5e8
BakaCookie520 <w222dft@outlook.com>
2026-02-18 10:11:19 +0800

    2026.2.18

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.core.utils.astrbot_path import get_astrbot_data_path
+
 
 @register("自定义帮助", "BakaCookie520", "自定义帮助信息", "1.0.0")
 class MyPlugin(Star):
@@ -11,13 +13,13 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     @filter.command("helps")
-    async def helloworld(self, event: AstrMessageEvent):
+    async def helps(self, event: AstrMessageEvent):
         """返回帮助内容""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        yield event.image_result("./helps.png")
+        yield event.image_result("/AstrBot/data/plugins/astrbot_plugin_customized_help/helps.png")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
